7b96cd282393d7934e78365005d0b0a14aaee272
Joel Gómez <nahuelgomez329@gmail.com>
2024-06-16 12:32:59 -0300

    Revert "drivers: cpufreq: Revert cpu_input_boost commits"
    This reverts commit 9d6c4821c73c50a7b60930e00a78ef0da7faf2b8.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2250,9 +2250,9 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		cpu_input_boost_kick();
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
