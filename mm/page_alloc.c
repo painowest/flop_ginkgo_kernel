fca987ad9bd81fcd199a961d227cdae1e0f1a172
Joel Gómez <nahuelgomez329@gmail.com>
2024-09-07 12:09:44 -0300

    mm: page_alloc: use correct devfreq node for trinket
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4229,7 +4229,7 @@ retry:
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
 	cpu_input_boost_kick_max(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 100);
 
 	reserve_flags = __gfp_pfmemalloc_flags(gfp_mask);
 	if (reserve_flags)
@@ -4287,7 +4287,7 @@ retry:
 		goto nopage;
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
