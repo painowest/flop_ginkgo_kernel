a9200caa6e7d107793abebccf6b51f4e095e26f0
Joel Gómez <nahuelgomez329@gmail.com>
2024-09-07 12:10:50 -0300

    kernel: fork: use correct devfreq node for trinket
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2236,7 +2236,7 @@ long _do_fork(unsigned long clone_flags,
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current))
 		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
