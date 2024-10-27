c846a696a280be54aac5f8bf29addee014eca38c
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-10-27 19:49:20 -0300

    fs: susfs4ksu: fix header paths
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- include/linux/sus_su.h
+++ include/linux/sus_su.h
@@ -1,8 +1,8 @@
 #ifndef __KSU_H_SUS_SU
 #define __KSU_H_SUS_SU
 
-#include "../../drivers/kernelsu/core_hook.h"
-#include "../../drivers/kernelsu/sucompat.h"
+#include "../../drivers/kernelsu/kernel/core_hook.h"
+#include "../../drivers/kernelsu/kernel/sucompat.h"
 
 int sus_su_fifo_init(int *maj_dev_num, char *drv_path);
 int sus_su_fifo_exit(int *maj_dev_num, char *drv_path);
