e9753ba7408902c3dc4ea0f3b1ec3a6ffc05e47e
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-10-27 20:27:33 -0300

    mm: page-writeback: tune values again
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -71,7 +71,7 @@ static long ratelimit_pages = 256;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 50;
+int dirty_background_ratio = 10;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -88,7 +88,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 99;
+int vm_dirty_ratio = 30;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
@@ -99,14 +99,14 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 60000; /* centiseconds */
+unsigned int dirty_writeback_interval = 3000; /* centiseconds */
 
 EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 
 /*
  * The longest time for which data is allowed to remain dirty
  */
-unsigned int dirty_expire_interval = 60000; /* centiseconds */
+unsigned int dirty_expire_interval = 3000; /* centiseconds */
 
 /*
  * Flag that makes the machine dump writes/reads and block dirtyings.
